0ce9037f334540bc4f91581b845d526ae9a40d6e
Sherzod <sherzodjonsadillaev@gmail.com>
2024-06-08 17:45:38 +0500

    PWA 💻

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+const withPWA = require('@ducanh2912/next-pwa').default({
+	dest: 'public',
+	cacheOnFrontEndNav: true,
+	reloadOnOnline: true,
+	aggressiveFrontEndNavCaching: true,
+	swcMinify: true,
+	disable: false,
+	workboxOptions: {
+		disableDevLogs: true
+	}
+})
+
 const nextConfig = { reactStrictMode: false }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
